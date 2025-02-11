3c1e8652eae9ac6bfef410b0c55d59db340d4fd7
Fino59 <slipdan@hotmail.com>
2025-02-11 08:25:08 -0300

    feat: Add new components to page module

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,9 +1,11 @@
+// Angular
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomeComponent } from './home/home.component';
+
+// Module
 import { RoutingModule } from './routing.module';
-
-
+import { HomeComponent } from './home/home.component';
+import { SharedModule } from "../shared/shared.module";
 
 @NgModule({
   declarations: [
@@ -11,7 +13,8 @@ import { RoutingModule } from './routing.module';
   ],
   imports: [
     CommonModule,
-    RoutingModule
-  ]
+    RoutingModule,
+    SharedModule
+]
 })
 export class PagesModule { }
